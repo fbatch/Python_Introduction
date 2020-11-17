225af213bc5b52cbc164df660a438bc9ae77762e
fbatch <fyodor.batchkala@gmail.com>
2020-11-17 16:03:55 +0300

    HW3

--- HW3/hw3ex6.py
+++ HW3/hw3ex6.py
@@ -9,9 +9,12 @@ def title_pro_self_made():
     words = input('Введите строку из слов: ').split()
     for word in words:
         for letter in word:
-            if ord(letter) in range(97, 122):
-                word_1 = chr(ord(word[0]) - 32) + word[1:]
-            else:
+            if ord(letter) not in range(97, 122):
                 word_1 = word
+                break
+            else:
+                word_1 = chr(ord(word[0]) - 32) + word[1:]
         a.append(word_1)
     print(' '.join(a))
+
+title_pro_self_made()
