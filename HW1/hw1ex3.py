cbb02ed9a979e730d187904a147493e91a83b0dc
fbatch <74175998+fbatch@users.noreply.github.com>
2020-11-19 17:35:59 +0300

    Delete hw1ex3.py

--- HW1/hw1ex3.py
+++
@@ -1,4 +0,0 @@
-# №3 Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-a = input('Введите своё число:\n')
-result = int(a) + int(str(a) + str(a)) + int(str(a) + str(a) + str(a))
-print(result)
