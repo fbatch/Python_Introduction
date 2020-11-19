4d140d376f6f26ff8a396245446d241c035e007b
fbatch <74175998+fbatch@users.noreply.github.com>
2020-11-19 17:46:54 +0300

    Add files via upload

---
+++ hw2ex3.1.py
@@ -0,0 +1,8 @@
+#Exercise 3 (dictionary)
+a = int(input('Введите целое число от 1 до 12: '))
+my_dict = {'winter': [1, 2, 12], 'spring': [3, 4, 5], 'summer': [6, 7, 8], 'autumn': [9, 10, 11]}
+while a < 1 or a > 12:
+        a = int(input('Пожалуйста, введите целое число от 1 до 12: '))
+for m in my_dict.keys():
+    if a in my_dict.get(m):
+        print(f"It's {m}!")
