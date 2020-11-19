60dd13a924ff56b3a2295bd175782a5a80042341
fbatch <74175998+fbatch@users.noreply.github.com>
2020-11-19 17:35:44 +0300

    Delete hw1ex1.py

--- HW1/hw1ex1.py
+++
@@ -1,8 +0,0 @@
-# №1 Поработайте с переменными.
-user_id = input('Enter your ID:\n')
-user_name = input('What is your name?\n')
-user_age = input('How old are you?\n')
-user_password = input('Enter your password:\n')
-
-print('Welcome, {}!\nYou can see your account data below:'.format(user_id))
-print('Age: {}; User name: {}; User ID: {}.'.format(user_age, user_name, user_id))
