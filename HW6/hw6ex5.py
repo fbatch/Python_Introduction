9da083273670f30a0b412fffd4032234a57d1c3b
fbatch <fyodor.batchkala@gmail.com>
2020-11-27 17:29:08 +0300

    HW6

--- HW6/hw6ex5.py
+++ HW6/hw6ex5.py
@@ -1,7 +1,33 @@
-# 5. Реализовать класс Stationery (канцелярская принадлежность).
-# Определить в нем атрибут title (название) и метод draw (отрисовка).
-# Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса
-# Pen (ручка), Pencil (карандаш), Handle (маркер).
-# В каждом из классов реализовать переопределение метода draw.
-# Для каждого из классов методы должен выводить уникальное сообщение.
-# Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+from termcolor import colored
+
+
+class Stationary:
+    title = 'Канцелярская принадлежность'
+
+    def draw(self):
+        print('Запуск отрисовки.')
+
+
+class Pen(Stationary):
+    def draw(self):
+        print(colored('Запуск ручки.', 'blue'))
+
+
+class Pencil(Stationary):
+    def draw(self):
+        print(colored('Запуск карандаша.', 'red'))
+
+
+class Handle(Stationary):
+    def draw(self):
+        print('\x1b[0;30;43m' + 'Запуск маркера.' + '\x1b[0m')
+
+
+a = Pen()
+a.draw()
+
+b = Pencil()
+b.draw()
+
+c = Handle()
+c.draw()
